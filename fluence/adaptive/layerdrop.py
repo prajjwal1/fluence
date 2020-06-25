90341a5c650fac6f5117e3f485f923a19e96946d
prajjwal1 <prajjwalin@protonmail.com>
2020-06-26 01:29:19 +0900

    fix nbdev_diff_nbs

--- fluence/adaptive/layerdrop.py
+++ fluence/adaptive/layerdrop.py
@@ -9,13 +9,17 @@ from torch import nn
 # Cell
 class Layerdrop(nn.Module):
     """
-    Implements Reducing Transformer Depth on Demand with Structured Dropout
-    [Paper](https://arxiv.org/abs/1909.11556)
+    Implements [Reducing Transformer Depth on Demand with Structured Dropout]
+    (https://arxiv.org/abs/1909.11556)
 
     Arguments:
         module_list (nn.ModuleList): List from which layers are to dropped.
         layers_to_drop (int): number of layers to drop
+
+    Returns:
+        feats: pruned features
     """
+
     def __init__(self, module_list, layers_to_drop):
         super(Layerdrop, self).__init__()
         self.module_list = module_list
@@ -32,14 +36,17 @@ class Layerdrop(nn.Module):
                     feats = layer(feats, mask)
         return feats
 
-
 # Cell
 class Layerdrop_Cross(nn.Module):
     """
     This method is useful when layerdrop has to be
     used in multi modal settings (visual and linguistic)
     features
+
+    Returns:
+        lang_feats, visn_feats: pruned features for language and vision modality
     """
+
     def __init__(self, module_list, layers_to_drop=2):
         super(Layerdrop_Cross, self).__init__()
         self.module_list = module_list
@@ -50,6 +57,7 @@ class Layerdrop_Cross(nn.Module):
         x = torch.randint(0, self.length, (self.layers_to_drop,))
         for index, layer in enumerate(self.module_list):
             if index not in x:
-                lang_feats, visn_feats = layer(lang_feats, lang_attention_mask,
-                                               visn_feats, visn_attention_mask) #
+                lang_feats, visn_feats = layer(
+                    lang_feats, lang_attention_mask, visn_feats, visn_attention_mask
+                )  #
         return lang_feats, visn_feats
