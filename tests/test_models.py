db04e04b7bee2a3001ef9c9323051e96399c4f96
prajjwal1 <prajjwalin@protonmail.com>
2020-07-30 19:39:24 +0900

    remove mem requirements

--- tests/test_models.py
+++ tests/test_models.py
@@ -20,7 +20,7 @@ class Test_Model(unittest.TestCase):
             overwrite_cache=True,
         )
         self.tokenizer = AutoTokenizer.from_pretrained(self.MODEL_ID)
-        self.train_dataset = GlueDataset(self.data_args, self.tokenizer)
+        self.dataset = GlueDataset(self.data_args, self.tokenizer, mode="dev")
 
     def test_hex(self):
         config = AutoConfig.from_pretrained(
@@ -28,13 +28,13 @@ class Test_Model(unittest.TestCase):
         )
         model_a = AutoModel.from_pretrained(self.MODEL_ID)
         model_b = CBOW(config)
-        config.batch_size = 64
+        config.batch_size = 2
         model = OrthogonalTransformer(model_a, model_b, config)
         dataloader = DataLoader(
-            self.train_dataset,
+            self.dataset,
             batch_size=config.batch_size,
             collate_fn=default_data_collator,
         )
         batch = next(iter(dataloader))
         output = model(**batch)[1]
-        self.assertEqual(output.shape, torch.Size([64, 3]))
+        self.assertEqual(output.shape, torch.Size([2, 3]))
