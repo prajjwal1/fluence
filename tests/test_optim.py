4ef915f0d846a54a3bfc6a6cfce53ed51e1331ca
prajjwal1 <prajjwalin@protonmail.com>
2020-08-30 14:46:24 +0900

    fix optim test

--- tests/test_optim.py
+++ tests/test_optim.py
@@ -10,13 +10,13 @@ class Test_Optim(unittest.TestCase):
         model = nn.Linear(8, 8)
         base_optim = torch.optim.Adam(model.parameters())
         optim = Lookahead(base_optim, k=5, alpha=0.8)
-        output = model(torch.rand(128, 3, 64, 64))
+        output = model(torch.rand(128, 3, 8, 8))
         optim.step()
 
     def test_lamb(self):
         model = nn.Linear(8, 8)
         optim = Lamb(model.parameters())
-        output = model(torch.rand(128, 3, 64, 64))
+        output = model(torch.rand(128, 3, 8, 8))
         optim.step()
 
 
