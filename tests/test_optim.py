b6512387aa03e5c8d94836a82ec069f3a8c5715a
prajjwal1 <prajjwalin@protonmail.com>
2020-08-30 14:40:58 +0900

    install transformers with pip

--- tests/test_optim.py
+++ tests/test_optim.py
@@ -5,25 +5,16 @@ from torch import nn
 
 from fluence.optim import Lamb, Lookahead
 
-class Model(nn.Module):
-    def __init__(self):
-        self.model = nn.Sequential(nn.Linear(8, 8),
-                                   nn.ReLU(),
-                                   nn.Linear(8, 8))
-
-    def forward(self, x):
-        return self.model(x)
-
 class Test_Optim(unittest.TestCase):
     def test_lookahead(self):
-        model = Model()
+        model = nn.Linear(8, 8)
         base_optim = torch.optim.Adam(model.parameters())
         optim = Lookahead(base_optim, k=5, alpha=0.8)
         output = model(torch.rand(128, 3, 64, 64))
         optim.step()
 
     def test_lamb(self):
-        model = Model()
+        model = nn.Linear(8, 8)
         optim = Lamb(model.parameters())
         output = model(torch.rand(128, 3, 64, 64))
         optim.step()
